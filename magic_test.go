bbd56e4e6b1f14ddcbda79e88c38e8d669180c83
Ramón Márquez <ramon.marquez@graylog.com>
2023-07-26 22:45:14 -0400

    feat(magic.go): add support for custom configs for the lookup function - Modify function's signature to accept additional parameters to control concurrency when looking up the file types - Maintain backwards compatibility by making the new Lookup call the now unexported lookup function with default values - Add LookupConfig struct and LookupWithConfig function to provide a way to extend the lookup function's behavior - Add LookupSync add a shorthand for LookupWithConfig where concurrency has been disabled

--- magic_test.go
+++ magic_test.go
@@ -24,3 +24,47 @@ func TestNestedLookup(t *testing.T) {
 	assert.Equal(t, fileType.Extension, "wav")
 
 }
+
+func TestLookupWithConfig(t *testing.T) {
+	payload := []byte{31, 139, 8, 0, 130, 139, 110, 100, 2, 255, 61, 143, 65, 14, 131, 32, 20, 68, 247}
+
+	workerCounts := []int{-1, 0, 1, 2, 10000}
+
+	lookupConfigs := func(counts []int) []LookupConfig {
+		configs := make([]LookupConfig, len(workerCounts)*2)
+		for i := 0; i < len(counts)*2; i++ {
+			configs[i] = LookupConfig{
+				WorkerCount: counts[i%len(counts)],
+			}
+			if i < len(counts) {
+				configs[i].ConcurrencyEnabled = true
+			}
+		}
+		return configs
+	}(workerCounts)
+
+	for _, config := range lookupConfigs {
+		fileType, err := LookupWithConfig(payload, config)
+		require.Nil(t, err)
+		assert.Equal(t, fileType.Extension, "gz")
+	}
+
+}
+
+func TestLookupSync(t *testing.T) {
+
+	fileType, err := LookupSync([]byte{0xa1, 0xb2, 0xc3, 0xd4, 0x00, 0x00, 0x00, 0x00})
+	require.Nil(t, err)
+
+	assert.Equal(t, fileType.Extension, "pcap")
+
+}
+
+func TestNestedLookupSync(t *testing.T) {
+
+	fileType, err := LookupSync([]byte{0x52, 0x49, 0x46, 0x46, 0, 0, 0, 0, 0x57, 0x41, 0x56, 0x45})
+	require.Nil(t, err)
+
+	assert.Equal(t, fileType.Extension, "wav")
+
+}
